5899d7aacd8a3538c18ce21c7ef24270f23c514a
tekeshitime <kyaputenn13@gmail.com>
2024-02-26 19:54:02 +0900

    firstcommit

---
+++ src/components/Main.js
@@ -0,0 +1,91 @@
+import React, { useState } from "react";
+import { DragDropContext, Draggable, Droppable } from "react-beautiful-dnd";
+import dummyData from "../dummyData";
+import Card from "./Card";
+
+const Main = () => {
+  const [data, setData] = useState(dummyData);
+  const onDragEnd = (result) => {
+    const { source, destination } = result;
+
+    if (source.droppableId !== destination.droppableId) {
+      //別のカラムにタスクが移動したとき
+      const sourceColIndex = data.findIndex((e) => e.id === source.droppableId);
+      const destinationColIndex = data.findIndex(
+        (e) => e.id === destination.droppableId
+      );
+
+      const sourceCol = data[sourceColIndex];
+      const destinationCol = data[destinationColIndex];
+
+      const sourceTask = [...sourceCol.tasks];
+      const destinationTask = [...destinationCol.tasks];
+
+      const [removed] = sourceTask.splice(source.index, 1);
+
+      //動かした後のカラムにタスクを追加
+      destinationTask.splice(destination.index, 0, removed);
+
+      data[sourceColIndex].tasks = sourceTask;
+      data[destinationColIndex].tasks = destinationTask;
+      setData(data);
+    } else {
+      //同じカラム内でのタスクの入れ替え
+      const sourceColIndex = data.findIndex((e) => e.id === source.droppableId);
+      const sourceCol = data[sourceColIndex];
+
+      const sourceTask = [...sourceCol.tasks];
+      const [removed] = sourceTask.splice(source.index, 1);
+      sourceTask.splice(destination.index, 0, removed);
+
+      data[sourceColIndex].tasks = sourceTask;
+      setData(data);
+    }
+  };
+
+  return (
+    <DragDropContext onDragEnd={onDragEnd}>
+      <div className="trello">
+        {data.map((section) => (
+          <Droppable key={section.id} droppableId={section.id}>
+            {(provided, snapshot) => (
+              <div
+                className="trello-section"
+                ref={provided.innerRef}
+                {...provided.droppableProps}
+              >
+                <div className="trello-section-title">{section.title}</div>
+                <div className="trello-section-content">
+                  {section.tasks.map((task, index) => (
+                    <Draggable
+                      draggableId={task.id}
+                      index={index}
+                      key={task.id}
+                    >
+                      {(provided, snapshot) => (
+                        <div
+                          ref={provided.innerRef}
+                          {...provided.draggableProps}
+                          {...provided.dragHandleProps}
+                          style={{
+                            ...provided.draggableProps.style,
+                            opacity: snapshot.isDragging ? "0.5" : "1",
+                          }}
+                        >
+                          <Card>{task.title}</Card>
+                        </div>
+                      )}
+                    </Draggable>
+                  ))}
+                  {provided.placeholder}
+                </div>
+              </div>
+            )}
+          </Droppable>
+        ))}
+      </div>
+    </DragDropContext>
+  );
+};
+
+export default Main;
